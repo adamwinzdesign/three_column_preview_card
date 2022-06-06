bfa0098790c6d167cb841ff3871a1ce451254e3c
adamwinzdesign <adamwinzdesign@gmail.com>
2022-06-05 20:50:30 -0500

    added card_data

---
+++ src/card_data.jsx
@@ -0,0 +1,22 @@
+import { bright_orange, dark_cyan, very_dark_cyan } from "./colors";
+
+export const card_data = [
+	{
+		icon: "icon-sedans.svg",
+		title: "Sedans",
+		text: "Choose a sedan for its affordability and excellent fuel economy.  Ideal for cruising in the city or on your next road trip.",
+		color: bright_orange,
+	},
+	{
+		icon: "icon-suvs.svg",
+		title: "SUVS",
+		text: "Take an SUV for its spacious interior, power, and versatility.  Perfect for your next family vacation nad off-road adventures.",
+		color: dark_cyan,
+	},
+	{
+		icon: "icon-luxury.svg",
+		title: "Luxury",
+		text: "Cruise in the best car brands without the bloated prices.  Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+		color: very_dark_cyan,
+	},
+];
