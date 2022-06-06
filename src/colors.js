5507d2d57b8c175a7cf044a0b92067bc52a4df4d
adamwinzdesign <adamwinzdesign@gmail.com>
2022-06-05 20:16:30 -0500

    init

---
+++ src/colors.js
@@ -0,0 +1,6 @@
+export const bright_orange = "hsl(31, 77%, 52%)";
+export const dark_cyan = "hsl(184, 100%, 22%)";
+export const very_dark_cyan = "hsl(179, 100%, 13%)";
+
+export const transparent_white = "hsla(0, 0%, 100%, 0.75)";
+export const very_light_gray = "hsl(0, 0%, 95%)";
